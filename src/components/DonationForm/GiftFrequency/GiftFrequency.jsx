12870e8c9947c8f7db310b6b9af5255a09c196cb
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-05-22 02:00:06 +0430

    Feat: Add GiftFrequency component

---
+++ src/components/DonationForm/GiftFrequency/GiftFrequency.jsx
@@ -0,0 +1,33 @@
+import classes from './GiftFrequency.module.css';
+
+const GiftFrequency = () => {
+  return (
+    <div className={classes.GiftFrequency}>
+      <header className={classes.Title}>
+        <h4>Gift frequency</h4>
+      </header>
+      <div className={classes.Frequency}>
+        <div className={classes.RadioWrapper}>
+          <input
+            className={classes.Radio}
+            id='monthly'
+            type='radio'
+            name='Frequency'
+          />
+          <label htmlFor='monthly'>Monthly</label>
+        </div>
+        <div className={classes.RadioWrapper}>
+          <input
+            className={classes.Radio}
+            type='radio'
+            id='one-time'
+            name='Frequency'
+          />
+          <label htmlFor='one-time'>One time</label>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GiftFrequency;

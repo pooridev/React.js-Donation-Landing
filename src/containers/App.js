6359dabffc5a02b7b6de7272b94b140ab7457385
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-05-22 02:06:27 +0430

    update: update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import DonationForm from '../components/DonationForm';
+import HelpUs from '../components/HelpUs';
+import classes from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className={classes.App}>
+      <HelpUs />
+      <DonationForm />
+    </main>
   );
 }
 

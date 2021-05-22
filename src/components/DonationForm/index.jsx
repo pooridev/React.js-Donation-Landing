844fb4423e6d35e8ead7160659cdce99a73ef6dc
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-05-22 19:47:41 +0430

    Fix: Fix some responsivity issue for devices with lest than 450px width

--- src/components/DonationForm/index.jsx
+++ src/components/DonationForm/index.jsx
@@ -1,12 +1,14 @@
 import GiftFrequency from './GiftFrequency/GiftFrequency';
 import classes from './DonationForm.module.css';
 import SelectAmount from './SelectAmount/SelectAmount';
+import Agreement from './Agreement';
 
 const DonationForm = () => {
   return (
     <form className={classes.DonationForm}>
       <GiftFrequency />
       <SelectAmount />
+      <Agreement />
     </form>
   );
 };

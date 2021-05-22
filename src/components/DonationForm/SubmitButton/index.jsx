36acd9c9a14eaa172f654948f09de937e3e5d79b
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-05-22 20:52:08 +0430

    Feat: add submit donation button

---
+++ src/components/DonationForm/SubmitButton/index.jsx
@@ -0,0 +1,7 @@
+import classes from './SubmitButton.module.css';
+
+const SubmitButton = () => (
+  <button className={classes.DonateButton}>Donate now</button>
+);
+
+export default SubmitButton;
--- src/components/DonationForm/index.jsx
+++ src/components/DonationForm/index.jsx
@@ -3,14 +3,16 @@ import classes from './DonationForm.module.css';
 import SelectAmount from './SelectAmount/SelectAmount';
 import Agreement from './Agreement';
 import NameInput from './NameInput';
+import SubmitButton from './SubmitButton';
 
 const DonationForm = () => {
   return (
-    <form className={classes.DonationForm}>
+    <form className={classes.DonationForm} onSubmit={e => e.preventDefault()}>
       <GiftFrequency />
       <SelectAmount />
       <Agreement />
       <NameInput />
+      <SubmitButton />
     </form>
   );
 };
